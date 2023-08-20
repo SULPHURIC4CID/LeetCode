5d031b52b2da0e2ecf2df7c0fe2f723a3c688bb2
SULPHURIC4CID <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-08-20 06:54:38 +0530

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,30 @@
+/*
+
+Author: Ananthanarayanan R
+
+Section: Algorithms
+Question: Linked List Class
+
+*/
+
+public class ListNode
+{
+    int val;
+	
+    ListNode next;
+	
+    ListNode() 
+	{
+		
+	}
+	
+    ListNode(int val) 
+	{	
+		this.val = val; 
+	}
+	
+    ListNode(int val, ListNode next) 
+	{
+		this.val = val; this.next = next; 
+	}
+}
