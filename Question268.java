f00e3bde8167f75057269fe1c2f680edfa24c34c
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-09-12 11:46:33 +0530

    Add files via upload

---
+++ Question268.java
@@ -0,0 +1,30 @@
+/*
+
+Author: Ananthanarayanan R
+
+Section: Algorithms
+Question: 268
+
+*/
+
+public class Question268
+{
+	public static int missingNumber(int[] nums)
+	{
+        int[] arr = new int[nums.length+1];
+		for(int i =0;i<nums.length;i++)
+		{
+			arr[nums[i]]++;
+		}
+		for(int i =0;i<arr.length;i++)
+			if(arr[i]==0)
+				return i;
+		return 0;
+    }
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method starts");
+	}
+}
+
