27935cc7fc922498e35d57d1a7bb0a3ba001c709
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-09-14 08:02:14 +0530

    Add files via upload

---
+++ Question707.java
@@ -0,0 +1,18 @@
+/*
+
+Author: Ananthanarayanan R
+
+Section: Algorithms
+Question: 707 - MyLinkedList
+
+*/
+
+public class Question707
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method starts.");
+		MyLinkedList obj = new MyLinkedList();
+		
+	}
+}
