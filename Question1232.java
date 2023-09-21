db1b49806cea96496d305759a324030f4054290d
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-09-21 09:19:45 +0530

    Add files via upload

---
+++ Question1232.java
@@ -0,0 +1,21 @@
+/*
+
+Author: Ananthanarayanan R
+
+Section: Algorithms
+Question: 1232
+
+*/
+
+public class Question1232
+{
+	public static boolean checkStraightLine(int[][] coordinates) 
+	{
+        
+    }
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method starts");
+		
+	}		
+}
