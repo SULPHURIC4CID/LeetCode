8001e0eedbf31a96e27d2bf2798b25228084791a
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-10-03 15:32:05 +0530

    Add files via upload

---
+++ Question303.java
@@ -0,0 +1,30 @@
+
+/*
+
+Author: Ananthanarayanan R
+
+Section: Algorithms
+Question: 303
+
+*/
+/*
+
+TestCases-
+
+
+
+*/
+
+class NumArray {
+    int[] nums;
+    public NumArray(int[] nums) {
+    this.nums = nums;    
+    }
+    
+    public int sumRange(int left, int right) {
+    int sum = 0;
+    for(int i = left;i<=right;i++)    
+        sum+=nums[i];
+    return sum;
+    }
+}
