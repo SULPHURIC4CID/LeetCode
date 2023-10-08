067ca9e4be5754ccbd1bf6908982a42158426728
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-10-08 23:01:57 +0530

    Add files via upload

---
+++ Question149.java
@@ -0,0 +1,31 @@
+/*
+
+Author: Ananthanarayanan R
+Section: Algorithms
+Question: Question149
+
+
+*/
+
+public class Question149
+{
+
+	public static int maxPoints(int[][] points) 
+	{
+        return 0;
+    }
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Main Method Starts");
+		int[][] points = {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
+		for(int i = 0;i<points.length;i++)
+		{
+			for(int j = 0;j<2;j++)
+			{
+				System.out.print(points[i][j]+" ");
+			}
+			System.out.println();
+		}
+	}
+}
