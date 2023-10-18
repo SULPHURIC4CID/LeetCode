79ba0f40bd0a71d1e23556ccc5739a28a1b9149b
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-10-18 15:40:22 +0530

    Add files via upload

---
+++ Question221.java
@@ -0,0 +1,25 @@
+/*
+
+Author: Ananthanarayanan R
+Section: Algorithms
+
+Question: 221
+
+
+*/
+
+public class Question221
+{
+	public static int maximalSquare(char[][] matrix) 
+	{
+        //Logic get all possible squares and find if square and area
+		
+		
+		
+    }
+	
+	public static void main(String[] args)
+	{
+		
+	}
+}
