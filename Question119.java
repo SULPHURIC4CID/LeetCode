fcfe50cc36a3aa5ff7a4063108916a838f2c2439
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-11-29 15:25:23 +0530

    Add files via upload

--- Question119.java
+++ Question119.java
@@ -1,44 +1,23 @@
-/*
-
-Author: Ananthanarayanan R
-Section: Algorithms
-Question: 11
-
-Description: Pascals Triangle 2
-
-*/
-
 public class Question119
 {
-	public static int[] generate(int numRows)
-	{
-		int[][] result = new int[numRows][];
-		int[] temp = {1};
-		result[0] = temp;
-		if(numRows == 1)
-			return temp;
-		for(int i = 2;i<=numRows;i++)
-		{
-			temp = new int[i];
-			for(int j = 0;j<i;j++)
-			{
-				if(j==0)
-					temp[j] = 1;
-				else if(j==i-1)
-					temp[j] = 1;
-				else
-					temp[j] = result[i-2][j-1] + result[i-2][j];
-			}
-			result[i-1] = temp;
-		}		
-		return result[numRows-1];
-	}
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Main method starts");
-		int numRows = 5;
-		int[] result = generate(numRows);
-		Array.display(result);
-	}
+    public static boolean check(TreeNode l,TreeNode r)
+    {
+        if(l==null && r ==null)
+            return true;
+        if(l == null || r== null || l.val!=r.val)
+            return false;
+        return check(l.left,r.right) && check(l.right,r.left);
+    }
+
+    public static boolean isSymmetric(TreeNode root) 
+    {
+        if(root == null)
+            return true;
+       return check(root.left,root.right);
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
 }
