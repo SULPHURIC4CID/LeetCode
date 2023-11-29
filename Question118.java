dddd660011972873bb2ea29080f1b273447e86b2
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-11-29 07:49:44 +0530

    Add files via upload

--- Question118.java
+++ Question118.java
@@ -1,45 +1,35 @@
-/*
-
-Author: Ananthanarayanan R
-Section: Algorithms
-Question: 118
-
-Description: Pascals Triangle
-
-*/
+import java.util.*;
 
 public class Question118
 {
-	public static int[][] generate(int numRows)
-	{
-		int[][] result = new int[numRows][];
-		int[] temp = {1};
-		result[0] = temp;
-		if(numRows == 1)
-			return result;
-		for(int i = 2;i<=numRows;i++)
-		{
-			temp = new int[i];
-			for(int j = 0;j<i;j++)
-			{
-				if(j==0)
-					temp[j] = 1;
-				else if(j==i-1)
-					temp[j] = 1;
-				else
-					temp[j] = result[i-2][j-1] + result[i-2][j];
-			}
-			result[i-1] = temp;
-		}		
-		return result;
-	}
-	
-	public static void main(String[] args)
-	{
-		System.out.println("Main method starts");
-		int numRows = 30;
-		int[][] result = generate(numRows);
-		for(int i = 0;i<result.length;i++)
-			Array.display(result[i]);
-	}
+    public static List<List<Integer>> generate(int numRows) 
+    {
+        List<List<Integer>> result = new ArrayList<>();
+        if(numRows == 0)
+            return result;
+        
+        List<Integer> row1 = new ArrayList<>();
+        row1.add(1);
+        result.add(row1);
+        for(int i = 1;i<numRows;i++)
+        {
+            List<Integer> temp = new ArrayList<>();
+            temp.add(1);
+            for(int j = 0;j<result.get(i-1).size()-1;j++)
+            {
+                List<Integer> temp2 = result.get(i-1);
+                temp.add(temp2.get(j)+temp2.get(j+1));
+            }
+            temp.add(1);
+            result.add(temp);
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        int numRows = 1;
+        List<List<Integer>> result = generate(numRows);
+        System.out.println(result);
+    }
 }
