c9e70cba4d5ca64a21cc502a47cf7478800b3749
Ananthanarayanan R <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-11-29 15:59:02 +0530

    Add files via upload

---
+++ Question104.java
@@ -0,0 +1,15 @@
+ public class Question104
+ {
+     public int maxDepth(TreeNode root) {
+     if (root == null)
+            return 0;
+        
+        int lDepth = maxDepth(root.left);
+        int rDepth = maxDepth(root.right);
+
+        if(lDepth>rDepth)
+            return lDepth + 1;
+        else
+            return rDepth + 1;   
+    }
+ }
